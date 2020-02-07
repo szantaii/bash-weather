9c4420b572394e89946b5b0da446cf664233f8e9
szantaii <szantaii@gmail.com>
2020-02-07 12:07:46 +0100

    Remove unnecessary empty line

--- change-unit-type.sh
+++ change-unit-type.sh
@@ -8,7 +8,6 @@ change_unit_type()
         printf "%s" "${unit_type}" > "${script_directory}/unit_type"
         temperature_unit="F"
         wind_unit="mph"
-
     else
         unit_type="metric"
         printf "%s" "${unit_type}" > "${script_directory}/unit_type"
