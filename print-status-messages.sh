ee5ec1e756b4e96e40688ff6a8643bee1fe62fdf
szantaii <szantaii@gmail.com>
2020-02-07 12:10:38 +0100

    Optimize writing into "screen buffer" file

--- print-status-messages.sh
+++ print-status-messages.sh
@@ -2,20 +2,18 @@
 
 print_init_message()
 {
-    local _message_text="Initializing bash-weather..."
-
     {
-    tput cup "${top_padding}" "${left_padding}"
+        tput cup "${top_padding}" "${left_padding}"
 
-    printf "%s" "${_message_text}"
-    } >> ${buffer}
+        printf "%s" "Initializing bash-weather..."
+    } >> "${buffer}"
 }
 
 print_refresh_message()
 {
-    local _message_text="Refreshing..."
-
     {
+        local _message_text="Refreshing..."
+
         tput cup $((top_padding + min_term_height - 1)) \
             $((left_padding + min_term_width - ${#_message_text}))
 
